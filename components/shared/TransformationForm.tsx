5876586fa90951c95844c0c2c68a674beb7485de
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-04-14 11:07:13 +0530

    Transformed image componet

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -30,6 +30,7 @@ import { useState, useTransition } from "react"
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils"
 import { updateCredits } from "@/lib/actions/user.actions"
 import MediaUploader from "./MediaUploader"
+import TransformedImage from "./TransformedImage"
 
 export const formSchema = z.object({
     title: z.string(),
@@ -108,10 +109,10 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
         setNewTransformation(null)
 
         startTransition(async () => {
-            // await updateCredits(
-            //     userId,
-            //     creditFee
-            // )
+            await updateCredits(
+                userId,
+                -1
+            )
         })
     }    
 
@@ -222,6 +223,14 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
                             type={type}
                         />
                     )} 
+                />
+                <TransformedImage
+                    image={image}
+                    type={type}
+                    title={form.getValues().title}
+                    isTransforming={isTransforming}
+                    setIsTransforming={setIsTransforming}
+                    transformationConfig={transformationConfig}
                 />
             </div>
 
